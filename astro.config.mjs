0191c1ed6cfde13ffe41a25b3188030a70d367bf
Sroyart <arthuriouzalen@gmail.com>
2025-04-08 19:07:16 +0200

    refactor: remove comment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  // Enable Svelte to support Svelte components.
   integrations: [],
   site: "portfolio.arthur.iouzalen.com",
 });
