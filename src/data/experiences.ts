45f99866078dee10ea711abf336a2d8771194dcf
Sroyart <arthuriouzalen@gmail.com>
2025-04-08 18:16:24 +0200

    refactor: about components

---
+++ src/data/experiences.ts
@@ -0,0 +1,35 @@
+const experiences = [
+  {
+    company: "Direction Générale de la Gendarmerie Nationale",
+    position: "Alternance - Développeur full stack",
+    missions: [
+      `Ajout de fonctionnalités sur les sites intranets existants, modification du front (ReactJS
+TypeScript, tailwind), modification des routes API (NestJS) et création de micro services
+(Docker, NestJS, HonoJS, Elasticsearch)`,
+      `Création, déploiement et développement d'une application web permettant la gestion
+d'un portefeuille de projets`,
+      `Participation à la partie hébergement des applications web`,
+      `Participation au recrutement de nouveaux collaborateurs`,
+      `Formation de nouveaux apprentis`,
+      `Maquettage et participation au recueil du besoin et à la création du cahier des charges`,
+    ],
+    date: "2023 - 2024",
+  },
+  {
+    company: "Numeriquest",
+    position: "Stage - Développeur front end",
+    missions: [
+      `Participation au développement d’un site à destination d’un client en HTML CSS`,
+      `Création d’un site vitrine pour l’entreprise`,
+    ],
+    date: "nov. 2020",
+  },
+  {
+    company: "Société Générale",
+    position: "Stage - Développeur informatique",
+    missions: ["Formation au language JAVA"],
+    date: "déc. 2018 - janv. 2019",
+  },
+];
+
+export default experiences;
---
+++ src/data/formations.ts
@@ -0,0 +1,27 @@
+const formations = [
+  {
+    company: "Sup de Vinci",
+    position: "Master - Spécialisation Développement full stack",
+    missions: [
+      `Cycle de vie de la conception d’un logiciel, test, qualité`,
+      `Exploitation de bases de données dans un projet Web`,
+      `Optimisation des performances – JAVA/GOLANG`,
+      `Optimisation de bases de données dans un backend – MongoDB`,
+      `Développement responsive – HTML5`,
+      `Introduction au design application UX`,
+    ],
+    date: "2022 - 2024",
+  },
+  {
+    company: "Sup de Vinci",
+    position: "Bachelor - Spécialisation Développement",
+    missions: [
+      `HTML / CSS / JS / PHP – Avancé`,
+      `Backend (Node.js, Prisma, etc.)`,
+      `Développement d’applications mobiles iOS & Android`,
+    ],
+    date: "2021 - 2022",
+  },
+];
+
+export default formations;
