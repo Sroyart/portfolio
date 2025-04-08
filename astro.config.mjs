6e02d7b234c0347e81ddede7f11366ff77f25bad
Sroyart <arthuriouzalen@gmail.com>
2025-04-08 19:09:27 +0200

    fix: Invalid url

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   integrations: [],
-  site: "portfolio.arthur.iouzalen.com",
+  site: "http://portfolio.arthur.iouzalen.com/",
 });
